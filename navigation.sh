0a334bc28db865a6fc6e5470673e45f3cae2a946
Fab <59670321+fabaguirre@users.noreply.github.com>
2024-11-20 03:47:15 -0500

    Add eza aliases with fallback to ls

--- navigation.sh
+++ navigation.sh
@@ -2,10 +2,20 @@ alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
 alias -='cd -'
-alias l='ls -CF'
-alias la='ls -A'
-alias ll='ls -alF'
-alias lla='ls -la'
 alias mkdir='mkdir -pv'
 alias rmd='rm -rf'
 
+# Check if eza is installed
+if command -v eza > /dev/null 2>&1; then
+  # Use eza for enhanced navigation
+  alias l='eza --color=always --color-scale=all --color-scale-mode=gradient --icons=always --group-directories-first'
+  alias ll='eza --color=always --color-scale=all --color-scale-mode=gradient --icons=always --group-directories-first -l -h'
+  alias la='eza --color=always --color-scale=all --color-scale-mode=gradient --icons=always --group-directories-first -a'
+  alias lla='eza --color=always --color-scale=all --color-scale-mode=gradient --icons=always --group-directories-first -a -l --git -h'
+else
+  # Fallback to standard ls aliases
+  alias l='ls -CF'
+  alias la='ls -A'
+  alias ll='ls -alF'
+  alias lla='ls -la'
+fi
